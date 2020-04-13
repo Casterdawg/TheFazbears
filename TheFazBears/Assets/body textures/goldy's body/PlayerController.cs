52bf9ef71b17b5f2e6be306d6d17496e2e62bb35
Casterdawg <40468898+Casterdawg@users.noreply.github.com>
2020-04-13 17:44:55 -0500

    Revert "ik_jump attempt"
    This reverts commit 82ec96c823552f91435abd7f468548d53243e0c3.

--- TheFazBears/Assets/body textures/goldy's body/PlayerController.cs
+++ TheFazBears/Assets/body textures/goldy's body/PlayerController.cs
@@ -4,103 +4,120 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 
-    public float walkSpeed = 2;
-    public float runSpeed = 6;
-    public float gravity = -12;
-    public float jumpHeight = 1;
-    [Range(0, 1)]
-    public float airControlPercent;
+	public float walkSpeed = 2;
+	public float runSpeed = 6;
+	public float gravity = -12;
+	public float jumpHeight = 1;
+	[Range(0, 1)]
+	public float airControlPercent;
 
-    public float turnSmoothTime = 0.2f;
-    float turnSmoothVelocity;
+	public float turnSmoothTime = 0.2f;
+	float turnSmoothVelocity;
 
-    public float speedSmoothTime = 0.1f;
-    float speedSmoothVelocity;
-    float currentSpeed;
-    float velocityY;
+	public float speedSmoothTime = 0.1f;
+	float speedSmoothVelocity;
+	float currentSpeed;
+	float velocityY;
 
-    Animator animator;
-    Transform cameraT;
-    CharacterController controller;
-    FootIK footKinetic;
+	Animator animator;
+	Transform cameraT;
+	CharacterController controller;
 
-    void Start()
-    {
-        animator = GetComponent<Animator>();
-        cameraT = Camera.main.transform;
-        controller = GetComponent<CharacterController>();
-        footKinetic = GetComponent<FootIK> () ;
-    }
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+		cameraT = Camera.main.transform;
+		controller = GetComponent<CharacterController>();
+	}
 
-    void Update()
-    {
-        // input
-        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        Vector2 inputDir = input.normalized;
-        bool running = Input.GetKey(KeyCode.LeftShift);
+	void Update()
+	{
+		// input
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		Vector2 inputDir = input.normalized;
+		bool running = Input.GetKey(KeyCode.LeftShift);
 
-        Move(inputDir, running);
+		Move(inputDir, running);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-{
-            Jump();
-        }
-        // animator
-        float animationSpeedPercent = ((running) ? currentSpeed / runSpeed : currentSpeed / walkSpeed * .5f);
-        animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Jump();
+		}
+		// animator
+		float animationSpeedPercent = ((running) ? currentSpeed / runSpeed : currentSpeed / walkSpeed * .5f);
+		animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
 
-    }
+	}
 
-    void Move(Vector2 inputDir, bool running)
-    {
-        if (inputDir != Vector2.zero)
-{
-            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
-            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, GetModifiedSmoothTime(turnSmoothTime));
-        }
+	void Move(Vector2 inputDir, bool running)
+	{
+		if (inputDir != Vector2.zero)
+		{
+			float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+			transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, GetModifiedSmoothTime(turnSmoothTime));
+		}
 
-        float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
-        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, GetModifiedSmoothTime(speedSmoothTime));
+		float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+		currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, GetModifiedSmoothTime(speedSmoothTime));
 
-        velocityY += Time.deltaTime * gravity;
-        Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
+		velocityY += Time.deltaTime * gravity;
+		Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
 
-        controller.Move(velocity * Time.deltaTime);
-        currentSpeed = new Vector2(controller.velocity.x, controller.velocity.z).magnitude;
+		controller.Move(velocity * Time.deltaTime);
+		currentSpeed = new Vector2(controller.velocity.x, controller.velocity.z).magnitude;
 
-        // if vertical movement is positive and kinetic is set to auto update and it says it's grounded then
-        // it hasn't registered movement to notice character is off the ground so force FixY execution now
-        if ((velocityY  > 0) && footKinetic.isGrounded() && footKinetic.automaticFixY)
-        {
-            footKinetic.FixY();
-        }
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+		if (controller.isGrounded)
+		{
+			velocityY = 0;
+		}
 
-        if (footKinetic.isGrounded())
-        {
-            velocityY = 0;
-        }
-    }
+=======
+=======
+>>>>>>> parent of 60927e5... Fix for jump issue: velocityY was being reset to 0 by the Move method on Update of the player controller before LateUpdate was called on the FootIK script which determines whether the character is grounded by FootIK resulting is velocity getting reset to 0 by the playercontroller when a jump had been initiated. The code checks if this condition will occur as a reslt of Update on the player controller and forces the footIK to fixY for the ground plane proactivly if required preventing velocity Y from being reset if a jump had just been initiated.
+=======
+>>>>>>> parent of 60927e5... Fix for jump issue: velocityY was being reset to 0 by the Move method on Update of the player controller before LateUpdate was called on the FootIK script which determines whether the character is grounded by FootIK resulting is velocity getting reset to 0 by the playercontroller when a jump had been initiated. The code checks if this condition will occur as a reslt of Update on the player controller and forces the footIK to fixY for the ground plane proactivly if required preventing velocity Y from being reset if a jump had just been initiated.
+=======
+>>>>>>> parent of 60927e5... Fix for jump issue: velocityY was being reset to 0 by the Move method on Update of the player controller before LateUpdate was called on the FootIK script which determines whether the character is grounded by FootIK resulting is velocity getting reset to 0 by the playercontroller when a jump had been initiated. The code checks if this condition will occur as a reslt of Update on the player controller and forces the footIK to fixY for the ground plane proactivly if required preventing velocity Y from being reset if a jump had just been initiated.
+		if (footKinetic.isGrounded())
+		{
+			velocityY = 0;
+		}
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> parent of 60927e5... Fix for jump issue: velocityY was being reset to 0 by the Move method on Update of the player controller before LateUpdate was called on the FootIK script which determines whether the character is grounded by FootIK resulting is velocity getting reset to 0 by the playercontroller when a jump had been initiated. The code checks if this condition will occur as a reslt of Update on the player controller and forces the footIK to fixY for the ground plane proactivly if required preventing velocity Y from being reset if a jump had just been initiated.
+=======
+>>>>>>> parent of 60927e5... Fix for jump issue: velocityY was being reset to 0 by the Move method on Update of the player controller before LateUpdate was called on the FootIK script which determines whether the character is grounded by FootIK resulting is velocity getting reset to 0 by the playercontroller when a jump had been initiated. The code checks if this condition will occur as a reslt of Update on the player controller and forces the footIK to fixY for the ground plane proactivly if required preventing velocity Y from being reset if a jump had just been initiated.
+=======
+>>>>>>> parent of 60927e5... Fix for jump issue: velocityY was being reset to 0 by the Move method on Update of the player controller before LateUpdate was called on the FootIK script which determines whether the character is grounded by FootIK resulting is velocity getting reset to 0 by the playercontroller when a jump had been initiated. The code checks if this condition will occur as a reslt of Update on the player controller and forces the footIK to fixY for the ground plane proactivly if required preventing velocity Y from being reset if a jump had just been initiated.
+=======
+>>>>>>> parent of 60927e5... Fix for jump issue: velocityY was being reset to 0 by the Move method on Update of the player controller before LateUpdate was called on the FootIK script which determines whether the character is grounded by FootIK resulting is velocity getting reset to 0 by the playercontroller when a jump had been initiated. The code checks if this condition will occur as a reslt of Update on the player controller and forces the footIK to fixY for the ground plane proactivly if required preventing velocity Y from being reset if a jump had just been initiated.
+	}
 
-    void Jump()
-    {
-        if (footKinetic.isGrounded())
-        {
-            float jumpVelocity = Mathf.Sqrt(-2 * gravity * jumpHeight);
-            velocityY = jumpVelocity;
-        }
-    }
+	void Jump()
+	{
+		if (controller.isGrounded)
+		{
+			float jumpVelocity = Mathf.Sqrt(-2 * gravity * jumpHeight);
+			velocityY = jumpVelocity;
+		}
+	}
 
-    float GetModifiedSmoothTime(float smoothTime)
-    {
-        if (footKinetic.isGrounded())
-        {
-            return smoothTime;
-        }
+	float GetModifiedSmoothTime(float smoothTime)
+	{
+		if (controller.isGrounded)
+		{
+			return smoothTime;
+		}
 
-        if (airControlPercent == 0)
-        {
-            return float.MaxValue;
-        }
-        return smoothTime / airControlPercent;
-    }
+		if (airControlPercent == 0)
+		{
+			return float.MaxValue;
+		}
+		return smoothTime / airControlPercent;
+	}
 }
