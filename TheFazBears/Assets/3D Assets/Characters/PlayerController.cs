1b1d0ce1ad5ad39e4255fe2fd7d35661eaf72f37
ElDuderini <44909968+ElDuderini@users.noreply.github.com>
2020-06-21 16:27:13 -0600

    Backup before upgrade

---
+++ TheFazBears/Assets/3D Assets/Characters/PlayerController.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float walkSpeed = 2;
+	public float runSpeed = 6;
+	public float gravity = -12;
+	[Range(0, 1)]
+	public float airControlPercent;
+
+	public float turnSmoothTime = 0.2f;
+	float turnSmoothVelocity;
+
+	public float speedSmoothTime = 0.1f;
+	float speedSmoothVelocity;
+	float currentSpeed;
+	float velocityY;
+
+	Animator animator;
+	Transform cameraT;
+	CharacterController controller;
+
+
+	[SerializeField] private AnimationCurve jumpFallOff;
+	[SerializeField] private float jumpMultiplier;
+
+	private bool isJumping;
+
+	private int jumpCount = 0;
+
+	//public Camera moveCamera;
+
+	//public GravityPower toggle;
+
+	//private bool aiming;
+
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+		cameraT = Camera.main.transform;
+		controller = GetComponent<CharacterController>();
+	}
+
+	void Update()
+	{
+		//if (Input.GetButton("Fire2"))
+		//{
+		//	aiming = true;
+		//}
+		//else if(!Input.GetButton("Fire2"))
+		//{
+		//	aiming = false;
+		//}
+
+
+		if (controller.isGrounded)
+		{
+			jumpCount = 0;
+		}
+
+		if(this.gameObject.name == "FoxyPants")
+		{
+			jumpCount = 1;
+		}
+
+
+		// input
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		Vector2 inputDir = input.normalized;
+		bool running = Input.GetKey(KeyCode.LeftShift);
+
+		Move(inputDir, running);
+
+		// animator
+		float animationSpeedPercent = ((running) ? currentSpeed / runSpeed : currentSpeed / walkSpeed * .5f);
+		animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+
+		JumpInput();
+
+
+	}
+
+	void Move(Vector2 inputDir, bool running)
+	{
+		if (inputDir != Vector2.zero)
+		{
+			float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+			transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, GetModifiedSmoothTime(turnSmoothTime));
+		}
+
+		float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+		currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, GetModifiedSmoothTime(speedSmoothTime));
+
+		velocityY += Time.deltaTime * gravity;
+		Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
+
+		controller.Move(velocity * Time.deltaTime);
+		currentSpeed = new Vector2(controller.velocity.x, controller.velocity.z).magnitude;
+
+		if (controller.isGrounded)
+		{
+			velocityY = 0;
+		}
+
+		
+
+	}
+
+	float GetModifiedSmoothTime(float smoothTime)
+	{
+		if (controller.isGrounded)
+		{
+			return smoothTime;
+		}
+
+		if (airControlPercent == 0)
+		{
+			return float.MaxValue;
+		}
+		return smoothTime / airControlPercent;
+	}
+
+	private void JumpInput()
+	{
+		if (Input.GetButtonDown("Jump") && !isJumping || Input.GetButtonDown("Jump") && jumpCount <= 1) 
+		{
+			animator.SetTrigger("hasJumped");
+			jumpCount++;
+			isJumping = true;
+			StopCoroutine(JumpEvent());
+			velocityY = 0;
+			StartCoroutine(JumpEvent());
+		}
+
+	}
+
+	private IEnumerator JumpEvent()
+	{
+		controller.slopeLimit = 90.0f;
+		float timeInAir = 0.0f;
+
+		do
+		{
+			float jumpForce = jumpFallOff.Evaluate(timeInAir);
+			controller.Move((Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime) / jumpCount);
+			timeInAir += Time.deltaTime;
+			yield return null;
+		} while (!controller.isGrounded && controller.collisionFlags != CollisionFlags.Above);
+
+		controller.slopeLimit = 45.0f;
+		isJumping = false;
+	}
+}
---
+++ TheFazBears/Assets/3D Assets/Characters/ThirdPersonCamera.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+
+	public bool lockCursor;
+	public float mouseSensitivity = 10;
+	public Transform target;
+	
+	public Vector2 pitchMinMax = new Vector2(-40, 85);
+	public float ScrollSensitvity = 2f;
+	public float ScrollDampening = 6f;
+	public float _CameraDistance = 10f;
+	public float rotationSmoothTime = .12f;
+	Vector3 rotationSmoothVelocity;
+	Vector3 currentRotation;
+
+	float yaw;
+	float pitch;
+
+	public bool aiming;
+
+	public GameObject aimPoint;
+
+	void Start()
+	{
+
+		//aimPoint = new Vector3(.5f, .6f, -1.3f);
+
+		if (lockCursor)
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+	}
+
+	private void Update()
+	{
+		if (Input.GetButton("Fire2"))
+		{
+			aiming = true;
+		}
+		else if (!Input.GetButton("Fire2"))
+		{
+			aiming = false;
+		}
+	}
+
+	void LateUpdate()
+	{
+		if (!aiming)
+		{
+			yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+			pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+			pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+
+			currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+			transform.eulerAngles = currentRotation;
+
+			transform.position = target.position - transform.forward * _CameraDistance;
+		}
+		else if(aiming)
+		{
+			//Vector3 localAdjustment = aimPoint - transform.position;
+
+			this.transform.position = Vector3.MoveTowards(transform.position, aimPoint.transform.position, .5f);
+			this.transform.eulerAngles = new Vector3(
+				Mathf.LerpAngle(transform.eulerAngles.x, 10, 1),
+				Mathf.LerpAngle(transform.eulerAngles.y, -5, 1),
+				Mathf.LerpAngle(transform.eulerAngles.z, 0, 1));
+		}
+		//if (Input.GetAxis("Mouse ScrollWheel") != 0f)
+		//{
+		//	float ScrollAmount = Input.GetAxis("Mouse ScrollWheel") * ScrollSensitvity;
+
+		//	ScrollAmount *= (this._CameraDistance * 0.3f);
+
+		//	this._CameraDistance += ScrollAmount * -1f;
+
+		//	this._CameraDistance = Mathf.Clamp(this._CameraDistance, 1.5f, 6f);
+		//}
+	}
+
+}
+
