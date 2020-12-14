b772d5500f5440da13350c9b0c778ee721c8ae24
ElDuderini <Justin_Peters@live.com>
2020-12-13 22:48:13 -0700

    Starting to implement AI

---
+++ TheFazBears/Assets/AI Test/AIState.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AIState
+{
+    public abstract void Update(GoldySC goldy);
+
+    public virtual void Enter(GoldySC goldy) { }
+
+    public virtual void Exit(GoldySC shareBear) { }
+}
---
+++ TheFazBears/Assets/ChaseAI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChaseAI : MonoBehaviour
+{
+    public GameObject player;
+    public Transform currentPos;
+    public NavMeshAgent agent;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
